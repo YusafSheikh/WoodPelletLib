1edb0da1b3d451f6af00771fdee058c66f790e05
yusaf <yusaf@DESKTOP-SCF39CV>
2023-05-29 23:41:39 +0200

    Add project files.

---
+++ WoodPelletLib/WoodPellet.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WoodPelletLib
+{
+    public class WoodPellet
+    {
+        public int Id { get; set; }
+        public string? Brand { get; set; } // ? angiver at Brand kan være null
+
+        public decimal Price { get; set; }
+        public int Quality { get; set; } // get set tillader læsning og skrivning af værdien
+
+        public void Validate() // metode som er ansvarlig for kontrol om alle krav er opfyldt
+        {
+            if (string.IsNullOrEmpty(Brand) || Brand.Length < 2) // tjekker om brand er null eller tom, eller har færre en 2 karakter
+            {
+                throw new ArgumentException("Brand skal have mindst 2 karaktere"); //Kaster argument exception hvis betingelsen ikke er opfyldt
+            }
+
+            if (Price <= 0) // tjekker om det er mindre eller lig med 0
+            {
+                throw new ArgumentException("Pris skal være et positivt nummer"); //Kaster argument exception hvis betingelsen ikke er opfyldt
+            }
+
+            if (Quality < 1 || Quality < 5) // tjekker om quality er mindre end 1 eller større end 5
+            {
+                throw new ArgumentException("Quality skal være mellem 1 til 5"); //Kaster argument exception hvis betingelsen ikke er opfyldt
+            }
+        }
+
+        public override string ToString() // Overskriver ToString() metoden fra baseklassen object
+        {
+            return $"(Id: {Id}, Brand: {Brand}, Price: {Price}, Quality: {Quality})"; // Returnerer en formateret streng, der repræsenterer objektet 
+        }
+    }
+}
---
+++ WoodPelletLibTests/WoodPelletTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WoodPelletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoodPelletLib.Tests
+{
+    [TestClass()]
+    public class WoodPelletTests
+    {
+        [TestMethod()]
+        public void BrandMinimumLengthOf2()
+        {
+            var woodPellet = new WoodPellet() { Brand = "A" }; //arrange
+
+            //Act and assert
+            Assert.ThrowsException<ArgumentException>(() => woodPellet.Validate());
+            // Forventer at en argument exception bliver kastet, hvis brand har mindre end 2 karakterer
+        }
+
+        [TestMethod()]
+        public void QualityBetween1_and_5()
+        {
+            var woodPellet = new WoodPellet { Quality = 6 }; // Arrange
+
+            Assert.ThrowsException<ArgumentException>(() => woodPellet.Validate());
+            // Forventer at ArgumentException bliver kastet hvis quality er mindre end 1 og større end 5
+            
+        }
+    }
+}
